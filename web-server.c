8d9f5ccdcbcd9474def0f545a8d8647a821e71dd
malisio <140167533+malisio@users.noreply.github.com>
2024-06-22 21:16:01 +0100

    Update web-server.c

--- web-server.c
+++ web-server.c
@@ -12,42 +12,50 @@
 
 int main(int argc, char const *argv[])
 {
-
+    //Declaration Of stuff we going to use later
     int socketfd;
     struct sockaddr_in server_addr ;
-    const char * OK_REQUEST ="HTTP/1.1 200 OK\r\n\r\n";
-
+    char * OK_REQUEST ="HTTP/1.1 200 OK\r\n\r\n";
+    //Creating a Socket
     socketfd=socket(AF_INET,SOCK_STREAM,0 ); 
-
+    //Defining The Struct of the socket to bind it 
     server_addr.sin_port=htons(PORT);
     server_addr.sin_family=AF_INET;
     server_addr.sin_addr.s_addr=   INADDR_ANY;
 
+    //Binding and checking if the Socket is binding without any Problems
 
     if(bind(socketfd,(const struct  sockaddr *) &server_addr, sizeof(server_addr)) < 0){
         printf("Binding Went Wrong!\n");
         exit(EXIT_FAILURE);
     } 
-
+    //Listening on the Port with the backlog being 3
     if(listen(socketfd, 3) < 0){
-
         printf("Listening Went Wrong!\n");
         exit(EXIT_FAILURE);
 
     }    
+    //Waiting For coonections
+
     printf("Waiting For connetion!\n");
 
-    accept(socketfd,(struct sockaddr * )&server_addr, 0 );
+    //Accepting the Connection and creating a  file descriptor
+    int server_addr_len= sizeof(socketfd);
+    
+    int fd = accept(socketfd,(struct sockaddr * )&server_addr,&server_addr_len);
 
-    if(send(socketfd,OK_REQUEST,strlen(OK_REQUEST),0) == -1){
-        printf("Message Sending Failed!\n");
-        exit(EXIT_FAILURE);
+    //Replying the request
 
-    }
-
+    int bytes_send = send(fd,OK_REQUEST,strlen(OK_REQUEST), 0);
     printf("Conneted!\n");
 
-    close(socketfd);
+    //Clossing and checking if something went Wrong
+
+    if(close(socketfd)  == -1 ){
+        printf("Closing the Socket went Wrong!\n");
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 
 }
